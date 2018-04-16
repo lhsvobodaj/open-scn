fa752b84cde49e2a80e8315a2e7fd8ec83e1d143
Luiz Svoboda <luiz.her.svoboda@hpe.com>
2018-04-15 23:10:27 -0300

    Add CSS SASS files with style and improe webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const webpack = require('webpack');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/js/index.js",
+  mode: 'development',
+  entry: './src/js/index.js',
   output: {
-    path: __dirname + "/dist/assets",
-    filename: "bundle.js",
-    publicPath: "assets"
+    filename: 'bundle.js'
   },
   devServer: {
     inline: true,
-    contentBase: "./dist",
+    contentBase: './dist',
     port: 3000
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-              "env",
-              "react",
-              "stage-2"
-            ]
+            presets: [ 'env', 'react', 'stage-2' ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.html$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true
           }
         }
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    })
+  ]
+};
