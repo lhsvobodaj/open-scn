6170f5177092c46d87e01cea600152ab1e068bd4
Luiz Svoboda <luiz.her.svoboda@hpe.com>
2018-04-22 09:29:05 -0300

    Execute some housekeeping

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 window.React = React;
 
 render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('react-app')
 );
