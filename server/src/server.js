c685549680a2867bfcac9e415d7d65997627d5c0
Luiz H. Svoboda Junior <luizek@gmail.com>
2018-09-29 23:34:58 +0400

    Add support for CORS

--- server/src/server.js
+++ server/src/server.js
@@ -32,6 +32,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/paper', papersRouter);
 app.use('/author', authorsRouter);
 app.use('/session', sessionRouter);
